7c3a291b64265a6dc4fd62d66b25170a2472abb9
Tushar Rathi (108) <170848832+TusharRathi108@users.noreply.github.com>
2025-09-19 11:18:36 +0530

    addded environment variable for backend api url

--- client/src/store/services/api.ts
+++ client/src/store/services/api.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApi = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3000/api/v1",
+        baseUrl: import.meta.env.VITE_API_URL,
         credentials: "include",
     }),
     tagTypes: ["BudgetHead", "BankHead", "User", "Project", "Proposal", "Location", "MLA", "Sector", "SubSectorWork"],
